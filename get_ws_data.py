09c337ea5c7200ba77dfe7b71928b17e2368ee31
akbar ali <akkuakbar97@gmail.com>
2022-04-24 16:43:22 +0400

    update: Adds documentation to get_ws_data file

--- get_ws_data.py
+++ get_ws_data.py
@@ -12,16 +12,21 @@ async def getData():
     async with websockets.connect(ws) as websocket:
         await websocket.send("")
         response = await websocket.recv()
+        # converts to json string to dictionary.
         response = json.loads(response)
+
+        # if current_data exists, appends the new response.
+        # Else, groups the data with timestamp as key.
+        # A new key is created for each minute.
         t = datetime.now()
         s = t.strftime('%Y-%m-%d %H:%M')
         current_data = ws_data.get(s, False)
-
         if current_data:
             current_data.append(response)
         else:
             ws_data[s] = [response]
 
+        # update the file with latest data.
         with open('data.json', 'w') as f:
             json.dump(ws_data, f, indent = 4)
 
@@ -29,6 +34,7 @@ def run():
     asyncio.run(getData())
 
 if __name__ == "__main__":
+    # creates 100 threads to get 100 response in 100ms.
     while True:
         threads = []
         for i in range(100):
